e9064b9b93900c4a3b0bbf7ba667cc4a71516579
Jade Allen Cook <jadeallencook@gmail.com>
2018-07-31 01:28:41 -0600

    🔗 Linked up navbar to enviroment to pull in pages for router

--- dev/src/app/components/navbar/navbar.component.ts
+++ dev/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  pages = environment.pages;
+
+  constructor() {}
 
   ngOnInit() {
   }
