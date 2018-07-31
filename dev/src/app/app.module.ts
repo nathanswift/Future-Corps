5e4b6a9629d6d06be8cdf42ecbd391694570f239
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-07-31 02:53:32 -0600

    compiled components

--- dev/src/app/app.module.ts
+++ dev/src/app/app.module.ts
@@ -5,6 +5,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PreloaderComponent } from './components/preloader/preloader.component';
+import { IntroParallaxComponent } from './components/intro-parallax/intro-parallax.component';
+import { HomeBannerComponent } from './components/home-banner/home-banner.component';
+import { OurProcessComponent } from './components/our-process/our-process.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AboutComponent } from './components/about/about.component';
+import { Parallax2Component } from './components/parallax2/parallax2.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { TeamComponent } from './components/team/team.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MapComponent } from './components/map/map.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const appRoutes: Routes = [
   { path: 'crisis-center', component: HomeComponent },
@@ -15,7 +30,22 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    PreloaderComponent,
+    IntroParallaxComponent,
+    HomeBannerComponent,
+    OurProcessComponent,
+    TestimonialsComponent,
+    ServicesComponent,
+    AboutComponent,
+    Parallax2Component,
+    ProfileComponent,
+    TimelineComponent,
+    TeamComponent,
+    PricingComponent,
+    ContactComponent,
+    MapComponent,
+    FooterComponent
   ],
   
   imports: [
---
+++ dev/src/app/components/intro-parallax/intro-parallax.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-parallax',
+  templateUrl: './intro-parallax.component.html',
+  styleUrls: ['./intro-parallax.component.css']
+})
+export class IntroParallaxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
