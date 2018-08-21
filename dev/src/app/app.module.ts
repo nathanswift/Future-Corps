de7cb6012f9d07aaa34d456c9b9e2d72fec7486a
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-08-21 03:05:16 -0600

    added text

--- dev/src/app/app.module.ts
+++ dev/src/app/app.module.ts
@@ -52,7 +52,8 @@ const appRoutes: Routes = [
     FooterComponent,
     ProjectsComponent,
     IntroVideoComponent,
-    VideoSliderComponent
+    VideoSliderComponent,
+    
   ],
   
   imports: [
